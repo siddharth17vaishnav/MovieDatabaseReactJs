3ef83e9aff92899ea181f3deac701bfbc3778334
siddharth17vaishnav <siddharth17vaishnav@gmail.com>
2021-04-23 10:04:01 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React ,{useState} from "react";
+import Search from './components/Search'
+import Results from './components/Results'
+import Popup from './components/Popup'
+import axios from 'axios'
+
+
+function App() {
+  const [ state, setState] = useState({
+    s:"",
+    results: [],
+    selected:{}
+});
+  const apiUrl = "https://www.omdbapi.com/?apikey=cd39e789";
+  const search =(e) =>{
+    if (e.key === "Enter") {
+      axios(apiUrl + "&s=" + state.s).then(({ data }) => {
+        let results = data.Search;
+
+        setState(prevState => {
+          return { ...prevState, results: results }
+        })
+      });
+    }
+  }
+  const handleInput =(e) => {
+    let s=e.target.value;
+    setState(prevState => {
+      return {...prevState,s: s}
+      
+    });
+    
+  }
+
+  const openPopup = id =>{
+    axios(apiUrl + "&i=" + id).then(({data})=>{
+      let result =data;
+console.log(result);
+      setState(prevState => {
+        return {...prevState,selected: result}
+      });
+    });
+  }
+
+  const closePopup = () =>{
+    setState(prevState => {
+      return {...prevState,selected:{}}
+    });
+  }
+  return (
+    <div className="App">
+      <header >
+       <h1>Movie Database</h1>
+      </header>
+      <main>
+        <Search handleInput={handleInput} search={search}/>
+        <Results results={state.results} openPopup={openPopup}/>
+        {(typeof state.selected.Title != "undefined") ? <Popup selected={state.selected} closePopup={closePopup} /> : false}
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
